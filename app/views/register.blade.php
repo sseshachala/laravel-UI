6711693c5e7f4ab1536034ac7e9f194b06e026c2
Sudhi <sid4it@gmail.com>
2014-08-31 00:00:25 -0500

    Registeration form

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -2,24 +2,24 @@
 
 @section('content')
 
-{{Form::open(array('url'=>'/register','method'=>'post'))}}
+{{Form::open(array(class="form-horizontal" 'url'=>'/register','method'=>'post' ))}}
 <h1>Register</h1>
-<div>
-	<input type="text" value="{{Input::old('email')}}" placeholder="Enter Email" required="" id="username" name="email"/>
+<div class="form-group">
+	<input type="text" class="input-xlarge" value="{{Input::old('email')}}" placeholder="Enter Email" required="" id="username" name="email"/>
 </div>
-<div>
-	<input type="text" value="{{Input::old('username')}}" placeholder="Enter Username" required="" id="username" name="username"/>
+<div class="form-group">
+	<input type="text" class="input-xlarge" value="{{Input::old('username')}}" placeholder="Enter Username" required="" id="username" name="username"/>
 </div>
-<div>
-	<input type="password"  placeholder="Enter Password" required="" id="password" name="password"/>
+<div class="form-group">
+	<input type="password"  class="input-xlarge" placeholder="Enter Password" required="" id="password" name="password"/>
 </div>
-<div>
-	<input type="password"  placeholder="Confirm Password" required="" id="password" name="password_confirmation"/>
+<div class="form-group">
+	<input type="password"  class="input-xlarge" placeholder="Confirm Password" required="" id="password" name="password_confirmation"/>
 </div>
-<div>
+<div class="form-group">
 	<input class="btn btn-primary" type="submit" value="Register" />
-	<a href="/forgotpassword">Forgot password?</a>
-	<a href="/login">Login</a>
+	<a class="btn btn-warning" href="/forgotpassword">Forgot password?</a>
+	<a class="btn btn-success" href="/login">Login</a>
 </div>
 {{Form::close()}}
 
