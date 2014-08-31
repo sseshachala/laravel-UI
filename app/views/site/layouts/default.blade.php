25ffe269dc6231192db77bfe01dee1a187cc17a1
Sudhi <sid4it@gmail.com>
2014-08-31 01:10:01 -0500

    Verbiages.

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -78,8 +78,8 @@
                         <li><a href="{{{ URL::to('user') }}}">Logged in as {{{ Auth::user()->username }}}</a></li>
                         <li><a href="{{{ URL::to('user/logout') }}}">Logout</a></li>
                         @else
-                        <li {{ (Request::is('user/login') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/login') }}}">Login</a></li>
-                        <li {{ (Request::is('user/register') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/create') }}}">{{{ Lang::get('site.sign_up') }}}</a></li>
+                        <li {{ (Request::is('user/login') ? ' class="active"' : '') }}><a href="{{{ URL::to('login') }}}">Login</a></li>
+                        <li {{ (Request::is('user/register') ? ' class="active"' : '') }}><a href="{{{ URL::to('register') }}}">{{{ Lang::get('site.sign_up') }}}</a></li>
                         @endif
                     </ul>
 					<!-- ./ nav-collapse -->
