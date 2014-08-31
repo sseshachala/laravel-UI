c41a8f6a3f97fbe6a2e10ee01560b8e4210e6d12
Sudhi <sid4it@gmail.com>
2014-08-31 00:04:05 -0500

    Title

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -7,17 +7,17 @@
 	<!--<![endif]-->
 	<head>
 		<meta charset="utf-8">
-		<title>KodeInfo Secure Login System</title>
+		<title>XervDock - Test drive Docker Images on Amazon AWS</title>
 		<!--
 		<link rel="stylesheet" type="text/css" href="styles.css" />
 		-->
 		<link rel="stylesheet" type="text/css" href="/zocial/zocial.css" />
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 		<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 		<style type="text/css">
 			.zocial{
 				width: 220px;
